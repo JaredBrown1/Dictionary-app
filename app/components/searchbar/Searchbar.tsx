c92fcef80ec10a1d4ca455f26c4d777fcafdf8cf
Jared Brown <jaredbrown1@hotmail.com>
2023-06-01 21:22:14 -0700

    basic search bar up

--- app/components/searchbar/Searchbar.tsx
+++ app/components/searchbar/Searchbar.tsx
@@ -1,4 +1,11 @@
 const Searchbar = () => {
-	return <div>Searchbar component</div>;
+	return (
+		<div className="w-full p-24 flex items-center justify-center">
+			<input
+				placeholder="Search for any word..."
+				className="w-3/5 h-16 rounded-xl bg-gray-100 p-4"
+			/>
+		</div>
+	);
 };
 export default Searchbar;
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Definition from './components/definition/Definition';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-12">
+		<main className="flex min-h-screen flex-col items-center p-12">
 			<Navbar />
 			<Searchbar />
 			<Definition />
